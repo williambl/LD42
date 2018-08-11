61722282b68988565b51238ec1ab8a774cbda7c6
williambl <william@williambl.com>
2018-08-11 10:59:07 +0100

    Add a player

---
+++ LD42/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    CharacterController controller;
+
+    // Use this for initialization
+    void Start () {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        float movementX = Input.GetAxis("Horizontal");
+        float movementZ = Input.GetAxis("Vertical");
+        controller.Move(new Vector3(movementX, 0, movementZ));
+    }
+}
