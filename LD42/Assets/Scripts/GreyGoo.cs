6b26eba28743650b41aed1414a8b9cbfd70e97b9
williambl <william@williambl.com>
2018-08-11 19:52:53 +0100

    Improve Grey Goo particles

--- LD42/Assets/Scripts/GreyGoo.cs
+++ LD42/Assets/Scripts/GreyGoo.cs
@@ -7,15 +7,23 @@ public class GreyGoo : MonoBehaviour {
     public float secondsUntilDoubled = 5;
 
     private float lastDoubleTime = 0;
+    private ParticleSystem particleSystem;
 
     // Use this for initialization
     void Start () {
+        particleSystem = GetComponent<ParticleSystem>();
     }
 
     // Update is called once per frame
     void Update () {
         if (Time.time > lastDoubleTime + secondsUntilDoubled) {
+            var emission = particleSystem.emission;
+            var main = particleSystem.main;
+            emission.rateOverTime = emission.rateOverTime.constant * 6/(transform.localScale.x/2);
+            main.startSize = main.startSize.constant * 2;
+
             transform.localScale *= 2;
+
             lastDoubleTime = Time.time;
         }
     }
