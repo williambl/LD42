95e949ba1abf2f4c6d3db9aac36308f6a3ea1f91
williambl <william@williambl.com>
2018-08-11 09:50:49 +0100

    Add basic grey goo

---
+++ LD42/Assets/Scripts/GreyGoo.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreyGoo : MonoBehaviour {
+
+    public float secondsUntilSplit = 1;
+
+    public GameObject prefab;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(Duplicate());
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    IEnumerator Duplicate () {
+        for (int i = 0; i < 20; i++) {
+            yield return new WaitForSeconds(secondsUntilSplit);
+            Instantiate(prefab, transform.position, transform.rotation);
+        }
+    }
+}
