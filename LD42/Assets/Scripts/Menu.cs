f83e9dbd9162d2503aa7f7ba19b1d78767ecfab9
williambl <william@williambl.com>
2018-08-12 19:09:42 +0100

    Add main menu

---
+++ LD42/Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public Button button;
+
+    void Start () {
+        button.onClick.AddListener(StartGame);
+    }
+
+    public void StartGame () {
+        SceneManager.LoadSceneAsync(1);
+    }
+}
